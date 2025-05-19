30292ebb7ee6ad032b5d0d5fce2b0f62b8b2c685
Antoine Honore <ahonore@protonmail.com>
2025-05-19 11:23:20 +0200

    compile

--- main_compx.py
+++ main_compx.py
@@ -186,7 +186,7 @@ def main(args):
         logger = TensorBoardLogger(log_dir, name=exp_name, default_hp_metric=False)
         os.makedirs(os.path.dirname(logger.log_dir), exist_ok=True)
         
-        #model = torch.compile(Predictor(hparams["model"]))
+        model = torch.compile(Predictor(hparams["model"]))
         model = Predictor(hparams["model"])
         
         ltrainer = lTrainer(model=model, hparams=hparams)
