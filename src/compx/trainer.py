9d3c3119e3a80c092f19f112210d9fa1d160f376
Antoine Honore <ahonore@protonmail.com>
2025-05-30 10:31:22 +0200

    minor

--- src/compx/trainer.py
+++ src/compx/trainer.py
@@ -71,8 +71,8 @@ class lTrainer(L.LightningModule):
         self.train_scores = {"y": [],   "yhat": [], "yclass":[], "norms": []}
         self.test_scores =  {"y": [],   "yhat": [], "yclass":[], "norms": []}
 
-
         self.cost_matrix = torch.tensor([[0,7,8,9,10], [200,0,7,8,9], [300,200,0,7,8], [400,300,200,0,7], [500,400,300,200,0]])
+        # 
         self.compute_confmat = ConfusionMatrix(task="multiclass", num_classes=self.cost_matrix.shape[-1])
     def configure_model(self):
         if self.model is not None:
@@ -212,7 +212,7 @@ class lTrainer(L.LightningModule):
         thescores["AUROC"+suffix] = multiclass_auroc(yhat, yclass, num_classes=yhat.shape[-1])
         thescores["AUPRC"+suffix] = multiclass_auprc(yhat, yclass, num_classes=yhat.shape[-1])
         cm = self.compute_confmat(yhat, yclass)
-        thescores["cost" + suffix] = (self.cost_matrix * cm).sum() / cm.sum()
+        thescores["cost" + suffix] = (self.cost_matrix.to(device=cm.device) * cm).sum() / cm.sum()
 
         thescores["topk2/exact"+ suffix] =   topk_multilabel_accuracy(yhat, y, criteria="exact_match", k=2)
         thescores["topk2/hamming"+ suffix] = topk_multilabel_accuracy(yhat, y, criteria="hamming", k=2)
