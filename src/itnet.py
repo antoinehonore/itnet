a6d5a15a13047686cd63f9b3d7144ee32609ae87
Antoine Honore <ahonore@protonmail.com>
2025-06-03 12:55:54 +0200

    minor

--- src/itnet.py
+++ src/itnet.py
@@ -22,8 +22,8 @@ def apply_domain1_normalization(m, t, eps=1e-5):
     if (m in cat_variables.keys()):
         # Normalize histograms
         t[...,:-2] = t[...,:-2] / (t[...,:-2].sum(-1).unsqueeze(-1)+eps)
-    else:
-        t = apply_log(t)
+    #else:
+    t = apply_log(t)
     return t
 
 
