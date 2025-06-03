7c4ca0971ac1cdaa628181a5e668885e29edd4de
Antoine Honore <ahonore@protonmail.com>
2025-06-03 11:39:07 +0200

    minor

--- src/itnet.py
+++ src/itnet.py
@@ -8,7 +8,7 @@ from src.compx.mydata import num_variables, cat_variables
 def apply_log(t, eps=1):
     return (t + eps).log()
 
-def apply_domain_normalization(m, t,eps=1e-5):
+def apply_domain_normalization(m, t, eps=1e-5):
     if (m in cat_variables.keys()):
         t[...,:-2] = t[...,:-2] / (t[...,:-2].sum(-1).unsqueeze(-1)+eps)
         t = apply_log(t)
