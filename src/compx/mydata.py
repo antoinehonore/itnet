5bc9f5b0f9101a5059f38dad3945456c3cb190cb
Antoine Honore <ahonore@protonmail.com>
2025-06-09 10:52:50 +0200

    minor

--- src/compx/mydata.py
+++ src/compx/mydata.py
@@ -171,8 +171,8 @@ def readouts2dict(readouts, tte, specs, root_dir=".",labels=None,dataset="traini
         for v_id, g in df.groupby("vehicle_id")
     }
     def get_vid_data(v_id,dd,specs):
-        numerics = {num_varname[0]: dataframe2X(dd[num_varname].copy())  for num_varname in num_variables}
-        categorical = {cat_varname:    dataframe2X(dd[cat_varnames].copy()) for cat_varname,cat_varnames in cat_variables.items()}
+        numerics = {num_varname[0]: dataframe2X(dd.copy()[num_varname])  for num_varname in num_variables}
+        categorical = {cat_varname:    dataframe2X(dd.copy()[cat_varnames]) for cat_varname,cat_varnames in cat_variables.items()}
         specs = {"specs": append_dummy_timeline(specs.copy()) }
         return {**numerics, **categorical, **specs}
 
